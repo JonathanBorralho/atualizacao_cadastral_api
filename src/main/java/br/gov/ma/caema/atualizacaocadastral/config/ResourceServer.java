4c22d2eff6cb7bb7822a5b7683c4230284b2d8c3
Jonathan Borralho <jonathan.borralho@gmail.com>
2021-01-29 10:16:31 -0300

    Configura Resource Server

--- src/main/java/br/gov/ma/caema/atualizacaocadastral/config/ResourceServer.java
+++
@@ -1,5 +0,0 @@
-package br.gov.ma.caema.atualizacaocadastral.config;
-
-public class ResourceServer {
-
-}
--- src/main/java/br/gov/ma/caema/atualizacaocadastral/config/WebSecurityConfig.java
+++ src/main/java/br/gov/ma/caema/atualizacaocadastral/config/WebSecurityConfig.java
@@ -1,12 +1,18 @@
 package br.gov.ma.caema.atualizacaocadastral.config;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +27,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		final SecretKey secretKey = new SecretKeySpec("as4d56as4d5as4d654as56d4as564d56as4d56a44da1819d7a".getBytes(), "HmacSHA256");
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 }
